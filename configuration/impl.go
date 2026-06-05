f79e5edfaad06792afdc6fe23cb9db4d61478c89
103sbavert <65838896+103sbavert@users.noreply.github.com>
2026-06-06 02:35:11 +0530

    feat(configuration): add JSON config loader

---
+++ configuration/impl.go
@@ -0,0 +1,29 @@
+package configuration
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+func (c *VideoConfig) GetAutomaticFilename() string {
+	return fmt.Sprintf("output_%s_%s_%s_%dfps_%bps", c.GradientType, *c.Output, c.Resolution, c.Framerate, c.Bitrate)
+}
+
+func LoadJsonFile(configPath *string) VideoConfig {
+	local_config := defaultConfig
+
+	file_bytes, err := os.ReadFile(*configPath)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	err = json.Unmarshal(file_bytes, &local_config)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	return local_config
+}
