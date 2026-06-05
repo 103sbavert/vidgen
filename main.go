7e95ed2572a77a1ffdf2099c0808f8ed872d23ce
103sbavert <65838896+103sbavert@users.noreply.github.com>
2026-06-06 02:36:46 +0530

    feat: initialize main.go with minimal template code

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"sbavert/vidgen/configuration"
+)
+
 func main() {
+	argument_list := os.Args
+
+	if len(argument_list) > 2 {
+		fmt.Println("Unknown arguments specified")
+		os.Exit(1)
+	}
+
+	config_json_file := os.Args[1]
+	abs_config_path, err := filepath.Abs(config_json_file)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	var config = configuration.LoadJsonFile(&abs_config_path)
+
+	fmt.Println(config)
 
 }
